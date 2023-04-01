57329c8bce280ef745358b6873625650f95dc7ef
Volodymyr Kryvonos <kruvonosvova@gmail.com>
2023-04-01 21:30:24 +0300

    Fixed build path algorithm

--- src/main/kotlin/lab2/Lab2.kt
+++ src/main/kotlin/lab2/Lab2.kt
@@ -1,22 +1,28 @@
 package lab2
 
 import readAdjacencyMatrixFromFile
-import java.io.File
 import java.util.Stack
 
 
 class Graph(private val matrix: Array<IntArray>) {
 
-    fun findEulerPath(isGraphDirected: Boolean = false): List<Pair<Int, Int>> {
+    data class MarkerableEdge(
+        val origin: Int,
+        val destination: Int,
+        var used: Boolean = false,
+    )
+
+    fun findEulerPath(isGraphDirected: Boolean = false): MutableList<MarkerableEdge> {
         val stack = Stack<Int>()
-        val path = mutableMapOf<Int, MutableList<Int>>()
+        val path = mutableMapOf<Int, MutableList<MarkerableEdge>>()
         stack.add(0)
         while (stack.isNotEmpty()) {
             val currentNode = stack.peek()
             var isHaveUnvisited = false
             for (i in 0 until matrix[currentNode].size) {
                 if (matrix[currentNode][i] != 0) {
-                    path[currentNode] = path[currentNode]?.apply { add(i) } ?: mutableListOf(i)
+                    path[currentNode] = path[currentNode]?.apply { add(MarkerableEdge(currentNode, i)) }
+                        ?: mutableListOf(MarkerableEdge(currentNode, i))
                     matrix[currentNode][i] = 0
                     if (!isGraphDirected) {
                         matrix[i][currentNode] = 0
@@ -30,29 +36,49 @@ class Graph(private val matrix: Array<IntArray>) {
                 stack.pop()
             }
         }
-        return sortPath(path)
+        val numberOfEdges = run {
+            var sum = 0
+            for (edge in path) {
+                sum += edge.value.size
+            }
+            sum
+        }
+        val res = buildPath(path, numberOfEdges)
+        if (res.size!=numberOfEdges || res.lastOrNull()?.destination!=res.firstOrNull()?.origin)
+            throw Exception("Path not found")
+        return res
     }
 
-    private fun sortPath(path: MutableMap<Int, MutableList<Int>>): List<Pair<Int, Int>> {
-        val sortedPath = mutableListOf<Pair<Int, Int>>()
-        var currentNode = 0
-        while (path.isNotEmpty()) {
-            val newNode = path[currentNode]?.removeLast() ?: throw Exception("Path not found")
-            sortedPath.add(Pair(currentNode + 1, newNode + 1))
-            if (path[currentNode]?.size == 0) {
-                path.remove(currentNode)
-            }
-            currentNode = newNode
+    private fun buildPath(
+        edgeList: MutableMap<Int, MutableList<MarkerableEdge>>,
+        numberOfEdges: Int,
+        currentVertex: Int = 0,
+        path: MutableList<MarkerableEdge> = mutableListOf()
+    ): MutableList<MarkerableEdge> {
+        for (edge in edgeList[currentVertex] ?: listOf()) {
+            if (edge.used)
+                continue
+            edge.used = true
+            path.add(edge)
+            if (path.size == numberOfEdges)
+                return path
+
+            val path = buildPath(edgeList, numberOfEdges, edge.destination, path)
+            if (path.size == numberOfEdges)
+                return path
+            path.removeLast()
+            edge.used = false
         }
-        return sortedPath
+        return path
     }
 }
+
 fun main() {
     val adjacencyMatrix = readAdjacencyMatrixFromFile("input_lab2.txt")
     val graph = Graph(adjacencyMatrix)
 
     println("Edges: ")
     println(graph.findEulerPath().joinToString("\n") {
-        "${it.first} --> ${it.second}"
+        "${it.origin} --> ${it.destination}"
     })
 }
